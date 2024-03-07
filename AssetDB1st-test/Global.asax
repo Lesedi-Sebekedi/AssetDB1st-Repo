2d2af12944fc9dfe2bc6948ca616b8dcc3470cc1
Eirenic <lsebekedi008@gmail.com>
2024-03-07 13:57:18 +0200

    Add project files.

---
+++ AssetDB1st-test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AssetDB1st_test.MvcApplication" Language="C#" %>
---
+++ AssetDB1st-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AssetDB1st_test.Startup))]
+namespace AssetDB1st_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
